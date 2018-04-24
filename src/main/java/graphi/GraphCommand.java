a9f79d0de19168bb8f09dee892b6e3b3fb7823fc
jRoadie <akter.hossain.ict@gmail.com>
2018-04-24 10:07:43 +0600

    try graph commands

---
+++ src/main/java/graphi/GraphCommand.java
@@ -0,0 +1,16 @@
+package graphi;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GraphCommand {
+
+  private static final Map<String, GraphCommand> COMMAND_GRAPH_CACHE = new HashMap<>();
+
+  public static class Builder {
+
+
+
+  }
+
+}
